b8924a6962da79c5d22bab2e3c24c2e043bb6c7e
gugletasucks <gugleta96@gmail.com>
2026-06-25 22:07:58 +0200

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -11,7 +11,7 @@ const PROJECTS = [
     platform: "youtube",
     embed: "CilpvYHEEO8",
     description: "Role: 1st AC, Video Editor, Colorist",
-    related: ["p02, p03"]
+    related: ["p02, p03, p04"]
   },
   {
     id: "p02",
@@ -21,7 +21,7 @@ const PROJECTS = [
     platform: "youtube",
     embed: "fNS6zZJ7zDM",
     description: "Role: 1st AC, Video Editor, Colorist",
-    related: ["p01, p03"]
+    related: ["p01, p03, p04"]
   },
   {
     id: "p03",
@@ -31,7 +31,17 @@ const PROJECTS = [
     platform: "youtube",
     embed: "f5gPtj8XD10",
     description: "Role: DP, 1st AC, Video Editor",
-    related: ["p01, p02"]
+    related: ["p01, p02, p04"]
+  },
+  {
+    id: "p04",
+    titleOverride: "We Got Your Back | CAKE.com",
+    type: "1ST AC · MOTION GRAPHICS DESIGNER",
+    thumb: "https://img.youtube.com/vi/DwakqUxIr6A/maxresdefault.jpg",
+    platform: "youtube",
+    embed: "DwakqUxIr6A",
+    description: "Role: 1st AC, Motion Graphics Designer",
+    related: ["p01, p02, p03"]
   }
   
 ];
