17058e59ada77f48d310bd2e2d2e843a0553f38f
gugletasucks <gugleta96@gmail.com>
2026-06-25 21:56:45 +0200

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -11,7 +11,7 @@ const PROJECTS = [
     platform: "youtube",
     embed: "CilpvYHEEO8",
     description: "Role: 1st AC, Video Editor, Colorist",
-    related: ["p02"]
+    related: ["p02, p03"]
   },
   {
     id: "p02",
@@ -21,7 +21,17 @@ const PROJECTS = [
     platform: "youtube",
     embed: "fNS6zZJ7zDM",
     description: "Role: 1st AC, Video Editor, Colorist",
-    related: ["p01"]
+    related: ["p01, p03"]
+  },
+  {
+    id: "p03",
+    titleOverride: "lockddd | Park The Bus DJ Set",
+    type: "DP · 1ST AC · VIDEO EDITOR",
+    thumb: "https://img.youtube.com/vi/f5gPtj8XD10/maxresdefault.jpg",
+    platform: "youtube",
+    embed: "f5gPtj8XD10",
+    description: "Role: DP, 1st AC, Video Editor",
+    related: ["p01, p02"]
   }
   
 ];
