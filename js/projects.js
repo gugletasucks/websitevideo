18558c89625a683ec0ec26f0b7a5197fe6795ece
gugletasucks <gugleta96@gmail.com>
2026-06-26 14:28:22 +0200

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -5,6 +5,16 @@
 const PROJECTS = [
   {
     id: "p01",
+    titleOverride: "Fantom - Devojka sa tužnim očima (Official Video)",
+    type: "DP · 1ST AC",
+    thumb: "https://img.youtube.com/vi/Mhnuxkgok60/maxresdefault.jpg",
+    platform: "youtube",
+    embed: "Mhnuxkgok60",
+    description: "Role: DP, 1st AC",
+    related: ["p02", "p03", "p01"]
+  },
+  {
+    id: "p02",
     titleOverride: "Keune Business Dinner at the Bristol",
     type: "1ST AC · VIDEO EDITOR · COLORIST",
     thumb: "https://img.youtube.com/vi/CilpvYHEEO8/maxresdefault.jpg",
@@ -14,27 +24,27 @@ const PROJECTS = [
     related: ["p02", "p03", "p04"]
   },
   {
-    id: "p02",
+    id: "p03",
     titleOverride: "Femme Affair Summer Season DJ Set",
     type: "1ST AC · VIDEO EDITOR · COLORIST",
     thumb: "https://img.youtube.com/vi/fNS6zZJ7zDM/maxresdefault.jpg",
     platform: "youtube",
     embed: "fNS6zZJ7zDM",
     description: "Role: 1st AC, Video Editor, Colorist",
-    related: ["p01, p03, p04"]
+    related: ["p01, p02, p04"]
   },
   {
-    id: "p03",
+    id: "p04",
     titleOverride: "lockddd | Park The Bus DJ Set",
     type: "DP · 1ST AC · VIDEO EDITOR",
     thumb: "https://img.youtube.com/vi/f5gPtj8XD10/maxresdefault.jpg",
     platform: "youtube",
     embed: "f5gPtj8XD10",
     description: "Role: DP, 1st AC, Video Editor",
-    related: ["p01", "p02", "p04"]
+    related: ["p01", "p02", "p03"]
   },
   {
-    id: "p04",
+    id: "p05",
     titleOverride: "We Got Your Back | CAKE.com",
     type: "1ST AC · MOTION GRAPHICS DESIGNER",
     thumb: "https://img.youtube.com/vi/DwakqUxIr6A/maxresdefault.jpg",
@@ -42,16 +52,6 @@ const PROJECTS = [
     embed: "DwakqUxIr6A",
     description: "Role: 1st AC, Motion Graphics Designer",
     related: ["p01", "p02", "p03"]
-  },
-  {
-    id: "p05",
-    titleOverride: "Fantom - Devojka sa tužnim očima (Official Video)",
-    type: "DP · 1ST AC",
-    thumb: "https://img.youtube.com/vi/Mhnuxkgok60/maxresdefault.jpg",
-    platform: "youtube",
-    embed: "Mhnuxkgok60",
-    description: "Role: DP, 1st AC",
-    related: ["p02", "p03", "p01"]
   }
   
 ];
